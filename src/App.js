85c26106ee5296647cee9127d20fbd9813609b34
Sachin Thadoda <sachinthadoda1729@gmail.com>
2024-05-07 10:46:10 +0530

    removed mdbreact import

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import NavBar from './components/Navbar/Navbar'
 import Login from './components/Login/Login'
 import SignUp from './components/SignUp/SignUp'
 import Welcome from './components/WelcomePage/WelcomePage'
-import SampleLogin from './components/Login/SampleLogin'
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/welcome" element={<Welcome />} />
-      <Route path="/sample" element={<SampleLogin />} />
     </Routes></>
   )
 }
